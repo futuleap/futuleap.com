75b46ba661ae5c55358c608545b87bb521dff425
siimveskilt <info@futuleap.com>
2023-06-04 14:21:09 +0300

    fix: responsiveness, accessibility, logo

--- apps/futuleap/app/page.tsx
+++ apps/futuleap/app/page.tsx
@@ -3,17 +3,24 @@ import Head from 'next/head';
 
 export default function Index() {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} role="main">
       <Head>
         <title>FUTULEAP</title>
+        <meta name="description" content="Welcome to FutuLeap, a future-forward company."/>
         <link rel="icon" href={`/favicon.ico`}/>
       </Head>
 
+      <header>
+        <figure className="logo">
+          <img src={`/images/logo.png`} alt="Logo of FutuLeap"/>
+        </figure>
+      </header>
       <main>
-        <div className="logo">
-          <img src={`/images/logo.png`} alt="FutuLeap Logo"/>
-        </div>
-        <div className="contact-email">info@futuleap.com</div>
+        <address aria-label="Contact FutuLeap at">
+          <a href="mailto:info@futuleap.com">
+            <strong>info@futuleap.com</strong>
+          </a>
+        </address>
       </main>
     </div>
   );
