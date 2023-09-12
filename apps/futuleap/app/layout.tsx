7d1ff450592289aa03be92e989f6d2946c8d8fb2
Siim Veskilt <siim.veskilt@gmail.com>
2023-09-12 13:41:04 +0300

    refactor: remove rocket

--- apps/futuleap/app/layout.tsx
+++ apps/futuleap/app/layout.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import './global.css';
 import '../public/styles/fonts.css';
-import dynamic from 'next/dynamic';
 
-
-const RocketComponent = dynamic(
-  () => import('./rocket-component'),
-  { ssr: false }
-);
 
 export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <>
       {children}
-      {/*<RocketComponent/>*/}
     </>
   );
 }
