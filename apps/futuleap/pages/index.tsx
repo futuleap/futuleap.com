0905c23727536d32245d0255e4be74112ff35f0c
Siim Veskilt <siim.veskilt@gmail.com>
2023-09-13 20:50:40 +0300

    feat: add copy button to the email text

--- apps/futuleap/pages/index.tsx
+++ apps/futuleap/pages/index.tsx
@@ -3,8 +3,17 @@ import RootLayout from '../app/layout';
 import SocialButton from '../components/social-button';
 import { EMAIL_PATH, INSTAGRAM_PATH, LINKEDIN_PATH } from '../components/icon-paths';
 import styles from './index.module.scss';
+import { useState } from 'react';
 
 export default function Index() {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopy = () => {
+    const email = 'info@futuleap.com';
+    navigator.clipboard.writeText(email);
+    setIsCopied(true);
+  };
+
   return (
     <RootLayout>
       <div className="container">
@@ -21,12 +30,20 @@ export default function Index() {
         </header>
         <main>
           <address aria-label="Contact FutuLeap at" className={styles.address}>
-            <a href="mailto:info@futuleap.com" aria-label="Send us an email at info@futuleap.com">
+            <a aria-label="Send us an email at info@futuleap.com">
               <strong>info@futuleap.com</strong>
             </a>
+            <button onClick={handleCopy} aria-label="Copy email address" className={styles.copyButton}>
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+                   style={{ color: isCopied ? '#5190e6' : '#00FBA9' }}>
+                <path d="M9 13H4V4a2 2 0 0 1 2-2h5v2H6v8h3v-1z" fill="currentColor"></path>
+                <path d="M3 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2v10z" stroke="currentColor"
+                      strokeWidth="2"></path>
+              </svg>
+            </button>
           </address>
 
-          <nav aria-label="Social Media Links" className="socialLinks">
+          <nav aria-label="Social Media Links" className={styles.socialLinks}>
             <SocialButton
               href="https://www.linkedin.com/company/futuleap/"
               label="LinkedIn"
